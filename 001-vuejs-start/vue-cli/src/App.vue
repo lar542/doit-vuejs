8a1dea90e2c00912e9f9d2270096fde217d6f964
gugu <lar542@naver.com>
2020-10-30 00:24:38 +0900

    인프런 강좌 : Vue.js 시작하기 - Age of Vue.js

---
+++ 001-vuejs-start/vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"
+      ></app-header>
+  </div>
+</template>
+
+<script>
+/* new Vue({
+  data: {
+    str: 'hi'
+  }
+}) */
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'hi!';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
